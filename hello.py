2985c1d8e3f7bb85250af3c770944a0b057e3b0f
Aderson Bertim <adersonbertim@gmail.com>
2023-09-26 01:17:23 -0300

    deleted

--- hello.py
+++
@@ -1,6 +0,0 @@
-# Input function, strip delete blank space, title make upper case in first letter
-name = input("What's your name? ").strip().title()
-# Split function make space between names
-first, last = name.split(" ")
-# Function print to print parameters using F string method.
-print(f"Hello, {last}!")
